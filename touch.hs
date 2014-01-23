39ccd04deab896b9fc2b1783f0b80f1d2f7559a8
Alexander Berntsen <alexander@plaimi.net>
2014-01-23 23:27:02 +0100

    Add touch

---
+++ touch.hs
@@ -0,0 +1,30 @@
+{-
+- Copyright (C) 2014 Alexander Berntsen <alexander@plaimi.net>
+-
+- This file is part of coreutilhs.
+-
+- bweakfwu is free software: you can redistribute it and/or modify
+- it under the terms of the GNU General Public License as published by
+- the Free Software Foundation, either version 3 of the License, or
+- (at your option) any later version.
+-
+- bweakfwu is distributed in the hope that it will be useful,
+- but WITHOUT ANY WARRANTY; without even the implied warranty of
+- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+- GNU General Public License for more details.
+-
+- You should have received a copy of the GNU General Public License
+- along with bwekfwu  If not, see <http://www.gnu.org/licenses/>.
+-} module Main where
+
+import Control.Monad (forM_)
+import System.Environment (getArgs)
+import System.IO (IOMode (WriteMode), hClose, openFile) 
+
+-- Make files at the passed in paths.
+main ::  IO ()
+main = do
+  paths <- getArgs
+  if null paths
+    then putStrLn "touch: missing file operand"
+    else forM_ paths $ \path -> openFile path WriteMode >>= hClose
