b7b8f95119c80a4312bf9036762dc3ae5ab8abcf
Allen Guo <guoguo12@gmail.com>
2014-01-31 16:10:13 -0500

    Add pwd

---
+++ pwd.hs
@@ -0,0 +1,25 @@
+{-
+- Copyright (C) 2014 Allen Guo <guoguo12@gmail.com>
+- Copyright (C) 2014 Alexander Berntsen <alexander@plaimi.net>
+-
+- This file is part of coreutilhs.
+-
+- coreutilhs is free software: you can redistribute it and/or modify
+- it under the terms of the GNU General Public License as published by
+- the Free Software Foundation, either version 3 of the License, or
+- (at your option) any later version.
+-
+- coreutilhs is distributed in the hope that it will be useful,
+- but WITHOUT ANY WARRANTY; without even the implied warranty of
+- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+- GNU General Public License for more details.
+-
+- You should have received a copy of the GNU General Public License
+- along with coreutilhs.  If not, see <http://www.gnu.org/licenses/>.
+-} module Main where
+
+import System.Directory (getCurrentDirectory)
+
+main ::  IO ()
+main = do
+  getCurrentDirectory >>= putStrLn
