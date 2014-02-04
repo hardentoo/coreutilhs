51a7bdca29090db471fc01757daecb95a0222c05
Alexander Berntsen <alexander@plaimi.net>
2014-02-04 10:51:15 +0100

    Add basename

---
+++ basename.hs
@@ -0,0 +1,30 @@
+{-
+- Copyright (C) 2014 Alexander Berntsen <alexander@plaimi.net>
+-
+- This file is part of coreutilhs.
+-
+- coreutilhs is free software: you can redistribute it and/or modify
+- it under the terms of the GNU General Public License as published by
+- the Free Software Foundation, either version 3 of the License, or
+- (at your option) any later version.
+-
+- coreutilhs is distributed in the hope that it will be useful,
+- but WITHOUT ANY WARRANTY; without even the implied warranty of
+- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+- GNU General Public License for more details.
+-
+- You should have received a copy of the GNU General Public License
+- along with coreutilhs.  If not, see <http://www.gnu.org/licenses/>.
+-} module Main where
+
+import Control.Monad (forM_)
+import System.Environment (getArgs)
+import System.FilePath.Posix (takeBaseName)
+
+-- Print filenames without directory and suffix.
+main ::  IO ()
+main = do
+  input <- getArgs
+  case input of
+    [] -> putStrLn "basename: missing file operand"
+    files -> forM_ files $ putStrLn . takeBaseName
