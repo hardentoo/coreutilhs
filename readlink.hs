7d114af7781ffd44e01a57910d3e709e0a8de2b5
Alexander Berntsen <alexander@plaimi.net>
2014-02-06 16:23:19 +0000

    Add readlink

---
+++ readlink.hs
@@ -0,0 +1,34 @@
+{-
+- Copyright (C) 2014 Alexander Berntsen <alexander@plaimi.net>
+-
+- This file is part of coreutilhs.
+-
+- coreutilhs is free software: you can redistribute it and/or modify
+- it under the terms of the GNU General Public License as published by
+- the Free Software Foundation, either version 3 of the License, or
+- (at your option) any later version.
+-
+- coreutilhs is distributed in the hope that it will be useful,
+- but WITHOUT ANY WARRANTY; without even the implied warranty of
+- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+- GNU General Public License for more details.
+-
+- You should have received a copy of the GNU General Public License
+- along with coreutilhs.  If not, see <http://www.gnu.org/licenses/>.
+-} module Main where
+
+import Control.Exception (catch)
+import Control.Monad (forM_)
+import System.Environment (getArgs)
+import System.Posix.Files (readSymbolicLink)
+
+-- Print values of symbolic links.
+main ::  IO ()
+main = do
+  input <- getArgs
+  case input of
+    [] -> putStrLn "readlink: missing file operand"
+    files -> forM_ files $ \l ->
+      readSymbolicLink l `catch` noLink >>= putStrLn
+  where noLink :: IOError -> IO FilePath
+        noLink _ = return ""
